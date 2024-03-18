1142df44c204eedd16fbabf16e879e169f3582ba
leo <leonard.aronsson@skola.taby.se>
2024-03-18 13:16:45 +0100

    api

--- hemsidaProjekt/script.js
+++ hemsidaProjekt/script.js
@@ -1,11 +1,29 @@
+let searchText = document.getElementById("txtSearch")
+
+searchText.onkeydown = async function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault()
+
+    let searchTerm = searchText.value 
+    console.log("Kommer söka efter", searchTerm)
+
+    let results = await search(searchTerm)
+    
+    renderResults(results)
+  }
+}
+
 async function search(searchString) {
-    let apiKey = "" 
+    let apiKey = "1a08c634ec1bc9d64558c15c3e88cdbf" 
     var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`
-    console.log("Den URL vi kommer anropa: ", url)
-  
     let response = await fetch(url)
-  
     let json = await response.json()
     return json
-  }
-  
+}
+
+async function renderResults(data) {
+    let resultDiv = document.getElementById("searchresults")
+    console.log("resultatet: ", data)
+    let results = await search("shrek");
+    renderResults(results);
+}
