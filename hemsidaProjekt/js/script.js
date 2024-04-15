f16778d07f177f619f36b7ce81401c99d857819b
leo <leonard.aronsson@skola.taby.se>
2024-04-15 13:14:42 +0200

    media queries + responsivity

--- hemsidaProjekt/js/script.js
+++ hemsidaProjekt/js/script.js
@@ -1,6 +1,15 @@
 let searchText = document.getElementById("txtSearch")
 
+async function search(searchString) {
+  let apiKey = '1a08c634ec1bc9d64558c15c3e88cdbf' 
+  var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`
+  let response = await fetch(url)
+  let json = await response.json()
+  return json
+}
+
 searchText.onkeydown = async function (event) {
+
   if (event.key === 'Enter') {
     event.preventDefault()
 
@@ -12,17 +21,10 @@ searchText.onkeydown = async function (event) {
   }
 }
 
-async function search(searchString) {
-  let apiKey = '1a08c634ec1bc9d64558c15c3e88cdbf' 
-  var url = `https://api.themoviedb.org/3/search/movie?query=${searchString}&api_key=${apiKey}`
-  let response = await fetch(url)
-  let json = await response.json()
-  return json
-}
 
 async function renderResults(data) {
   let resultDiv = document.getElementById('searchresults')
-  console.log('resultatet: ', data)
+  console.log('result: ', data)
   data.results.forEach(element => {
     let movieDiv = document.createElement('div')
     movieDiv.classList.add('moviediv')
@@ -33,8 +35,8 @@ async function renderResults(data) {
     ${element.title}
     `
     movieDiv.addEventListener('click', (e) => {
-
-    });
+      window.location.href='../html/movie.html'
+    })
     resultDiv.appendChild(movieDiv)
   })
 }
